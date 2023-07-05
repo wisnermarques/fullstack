895928b9f08946d9ac6a54d7a5ec60d7346fc8ed
Wisner Marques <wisner.a.marques@gmail.com>
2023-07-05 15:53:04 -0300

    part2

---
+++ part2b-exercises/src/App.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (!newName || !newNumber) {
+      alert("Nome ou número não informado!");
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+      };
+
+      const existName = persons.find((person) => person.name === newName);
+      if (existName) {
+        alert(`${personObject.name} já foi adicionado à lista telefônica!`);
+      } else {
+        setPersons(persons.concat(personObject));
+        setNewName("");
+        setNewNumber("");
+      }
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const filteredPersons = filter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : persons;
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <h2>Add a new</h2>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={filteredPersons} />
+    </div>
+  );
+};
+
+export default App;
