0e3c9fa2e35fb1e8a19d7583912f75c9f8196a98
Wisner Marques <wisner.a.marques@gmail.com>
2023-07-06 11:47:43 -0300

    part2d post

---
+++ part2d/src/App.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import Note from "./components/Note";
+
+const App = (props) => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log("effect (efeito)");
+    fetch("http://localhost:3001/notes", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log("promise fulfilled (promessa resolvida)");
+        setNotes(data);
+      });
+  }, []);
+  console.log("render (renderiza)", notes.length, "notes (notas)");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+
+    fetch("http://localhost:3001/notes", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(noteObject),
+    }).then((resp) => {
+      resp.json().then((data) => setNotes(notes.concat(data)));
+    });
+
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
