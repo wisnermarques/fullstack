3a85546f55e3900bb80d7f9b75bd759ec75aab39
Wisner Marques <wisner.a.marques@gmail.com>
2023-07-19 13:56:11 -0300

    test unit

--- part4b/mongo.js
+++ part4b/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://wisnermarques:${password}@cluster0.uvdy6ny.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://wisnermarques:${password}@cluster0.uvdy6ny.mongodb.net/testNoteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
