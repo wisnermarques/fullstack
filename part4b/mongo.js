cf362daf808d1871152adf35f0c6a19e8021875c
Wisner Marques <wisner.a.marques@gmail.com>
2023-07-19 11:07:03 -0300

    test

---
+++ part4b/mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://wisnermarques:${password}@cluster0.uvdy6ny.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Easy',
+  important: true,
+})
+
+note.save().then((result) => {
+  console.log('note saved!' + result)
+  mongoose.connection.close()
+})
